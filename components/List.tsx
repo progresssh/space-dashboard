c6c0c457f7b9e06776220bb3aaaa801da0dcd1f9
Progress <star@progress.sh>
2022-11-06 20:34:40 +0100

    added query of spaceX API and clientside fetching for details

--- components/List.tsx
+++ components/List.tsx
@@ -1,12 +1,25 @@
+import { Dispatch, SetStateAction } from "react"
 import { LaunchesInterface } from "../interfaces/launches"
+import { LaunchesQueryInterface } from "../interfaces/launchesQuery"
+import Item from "./Item"
+interface ListInterface {
+    data: LaunchesQueryInterface
+    setActiveItem: Dispatch<SetStateAction<LaunchesInterface | null>>
+}
 
-function List({ data }: { data: LaunchesInterface[] }) {
+function List({ data, setActiveItem }: ListInterface) {
     return (
         <div>
             <ul>
-                {data?.map((item) => (
-                    <li key={item.id}>{item.name}</li>
-                ))}
+                {data.docs.map((item) => {
+                    return (
+                        <Item
+                            key={item.id}
+                            item={item}
+                            setActiveItem={setActiveItem}
+                        />
+                    )
+                })}
             </ul>
         </div>
     )
